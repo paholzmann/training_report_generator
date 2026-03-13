cc6f073f346727549812b1bb67c4fd420ed48df1
paholzmann <patrickholzmann4@gmail.com>
2026-03-13 16:14:01 +0100

    Create JSON Utilities with functions: loading strava api secrets from json config file and inserting strava api secrets into json config file

--- src/utilities.py
+++ src/utilities.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv, dotenv_values
 
 class ENV_Utilities:
@@ -9,12 +10,24 @@ class ENV_Utilities:
         load_dotenv()
         return {
             "base_url": os.getenv("BASE_URL"),
-            "clint_id": os.getenv("CLIENT_ID"),
+            "token_url": os.getenv("TOKEN_URL"),
+            "client_id": os.getenv("CLIENT_ID"),
             "client_secret": os.getenv("CLIENT_SECRET"),
-            "access_token": os.getenv("ACCESS_TOKEN"),
-            "refresh_token": os.getenv("REFRESH_TOKEN")
             }
 
+
+class JSON_Utilities:
+    def __init__(self):
+        pass
+
+    def load_json_secrets(self) -> dict[str, str]:
+        with open("configs/tokens.json", "r", encoding="utf-8") as f:
+            json_secrets = json.load(f)
+        return json_secrets
+
+    def insert_json_secrets(self, json_secrets: dict[str, str]) -> None:
+        with open("configs/tokens.json", "w", encoding="utf-8") as f:
+            json.dump(json_secrets, f, indent=4)
 
 if __name__ == "__main__":
     print(ENV_Utilities().load_env_variables())
